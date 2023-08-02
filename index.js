e5d3a7bf59126cc5dd09d55c281a2e297b94b0b9
YADengUU <yaqi.deng@igp.uu.se>
2023-08-02 20:37:34 +0800

    #1 localstorage

--- index.js
+++ index.js
@@ -13,11 +13,16 @@ const addTask = () => {
 listContainer[0].addEventListener("click", function (e) {
     if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
+        saveData()
     } else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
+        saveData()
     }
 }, false);
 
+const saveData = () => {
+    localStorage.setItem('data', listContainer[0].innerHTML)
+}
 const showTasks = () => {
     listContainer[0].innerHTML = localStorage.getItem('data')
 }
